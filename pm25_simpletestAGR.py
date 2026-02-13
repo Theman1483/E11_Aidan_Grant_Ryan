a250c33288ecaed37979b870cbfd581cff375b14
Grant Silva <grantsilva808@berkeley.edu>
2026-02-13 15:11:37 -0800

    Update pm25_simpletestAGR.py

--- pm25_simpletestAGR.py
+++ pm25_simpletestAGR.py
@@ -47,7 +47,7 @@ pm25 = PM25_UART(uart, reset_pin)
 
 print("Found PM2.5 sensor, reading data...")
 
-file = open('simpletestdata/simpletest.csv', 'w', newline = None)
+file = open('data/simpletest.csv', 'w', newline = None)
 
 csvwriter = csv.writer(file, delimiter=',')
 
