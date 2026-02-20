613c593f2ace9a5a53e4fb5aa084b77c86db89af
Grant Silva <grantsilva808@berkeley.edu>
2026-02-20 15:09:37 -0800

    Update pm25_simpletestAGR.py

--- pm25_simpletestAGR.py
+++ pm25_simpletestAGR.py
@@ -112,7 +112,7 @@ for i in range(30):
     print("---------------------------------------")
     print("Weather Data")
     print("---------------------------------------")
-    #print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
+    print("\nTemperature: %0.1f C" %bme680.temperature)
     print("Gas: %d ohm" %bme680.gas)
     print("Humidity : %0.1f %%" %bme680.relative_humidity)
     print("Pressure: %0.3f hPa" %bme680.pressure)
