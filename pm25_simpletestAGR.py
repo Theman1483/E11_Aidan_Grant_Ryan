805825e069459ce08c7191f16043f2cbb3aa5bed
Grant Silva <grantsilva808@berkeley.edu>
2026-02-20 15:03:03 -0800

    Update pm25_simpletestAGR.py

--- pm25_simpletestAGR.py
+++ pm25_simpletestAGR.py
@@ -112,7 +112,7 @@ for i in range(30):
     print("---------------------------------------")
     print("Weather Data")
     print("---------------------------------------")
-    print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
+    #print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
     print("Gas: %d ohm" %bme680.gas)
     print("Humidity : %0.1f %%" %bme680.relative_humidity)
     print("Pressure: %0.3f hPa" %bme680.pressure)
@@ -124,7 +124,7 @@ for i in range(30):
     now = time.time()
     env = aqdata["pm25 env"]
     particles = aqdata["particles 25um"]
-    temp = (%bme680.temperature + temperature_offset)
+    #temp = (%bme680.temperature + temperature_offset)
     gas = %bme680.gas
     humidity = %bme680.relative_humidity
     pressure = %bme680.pressure
