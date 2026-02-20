d9e762c49adc05401276cebc4464cfe5ec213adf
Grant Silva <grantsilva808@berkeley.edu>
2026-02-20 14:38:26 -0800

    Create weather-checkpoint.py

---
+++ .ipynb_checkpoints/weather-checkpoint.py
@@ -0,0 +1,30 @@
+import adafruit_bme680
+import time
+import board
+import pandas  as pd
+import numpy as np
+
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+bme680.sea_level_pressure = 1013.25
+
+temp_offset = -5
+i=0
+print("Time = %0.2f" %time.time())
+temp = []
+while i < 10:
+	
+	temp.append(bme680.temperature)
+	print("Gas: %d ohm" %bme680.gas)
+	print("Humidity : %0.1f %%" %bme680.relative_humidity)
+	print("Pressure: %0.3f hPa" %bme680.pressure)
+	print("Altitude = %0.2f meters" % bme680.altitude)
+	print("Time = %0.2f" %time.time())
+	i+=1
+	
+	
+	time.sleep(.2)
+ 
+print("Temperature: ")
+print(temp)
