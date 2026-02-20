0f123fdbb83e14534f682a8086e11427483852aa
Grant Silva <grantsilva808@berkeley.edu>
2026-02-20 15:10:48 -0800

    Update pm25_simpletestAGR.py

--- pm25_simpletestAGR.py
+++ pm25_simpletestAGR.py
@@ -54,7 +54,7 @@ file = open('data/simpletest.csv', 'w', newline = None)
 
 csvwriter = csv.writer(file, delimiter=',')
 
-meta = ['time','PM2.5 env','particles 2.5um']
+meta = ['time','PM2.5 env','particles 2.5um','Temperature','Gas','Humidity','Pressure','Altitude']
 csvwriter.writerow(meta)
 
 # for i in range(10):
